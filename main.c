a929a062c1dc1d7a98fd8545dc28bb445cbd8b3f
Santiagomorel <smorel@frba.utn.edu.ar>
2022-04-29 15:08:20 -0300

    Trabajo Práctico 1 Completo

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
 
-
-int main(int argc, char const *argv[])
+int main()
 {
-    /* code */
+    int c;    
+    while ((c = getchar()) != EOF)
+    {
+        if (isupper(c)) // COMPRUEBA SI EL CARACTER ESTA EN MAYÚSCULAS
+        {
+            putchar(tolower(c)); // COLOCA EL CARACTER EN MINÚSCULAS
+        }
+        if (islower(c)) // COMPRUEBA SI EL CARACTER ESTA EN MINÚSCULAS
+        {
+            putchar(toupper(c)); // COLOCA EL CARACTER EN MAYÚSCULAS
+        }
+        if (ispunct(c)) // COMPRUEBA SI EL CARACTER ES UN CARACTER DE PUNTUACION
+        {
+            putchar(c); // COLOCA EL DIGITO COMO ESTA
+        }
+    }
     return 0;
 }
